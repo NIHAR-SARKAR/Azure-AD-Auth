c2dcbc8ff6c629ca39c36ce2e2623ebf50234b5b
NIHAR_SARKAR <rana.sarkar61@gmil.com>
2022-06-04 04:02:39 +0530

    update

---
+++ AzureAdWebX/AzureAdWebX/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AzureAdWebX.Startup))]
+
+namespace AzureAdWebX
+{
+    public class Startup
+    {
+        //public Iconfi MyProperty { get; set; }
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
